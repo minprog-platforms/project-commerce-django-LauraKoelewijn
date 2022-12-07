d9a83dc38ffc1b20e0e3770f8e9f1594e0624bbb
MacBook Pro <macbookpro@MacBook-Pro-van-MacBook.local>
2022-12-07 13:27:16 +0100

    Added AuctionListing model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField(max_length=600)
+    current_price = models.FloatField()
+    photo = models.URLField()
+    category = models.CharField(max_length=20)
